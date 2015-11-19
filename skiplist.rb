7d54a4d429b2adb2befef3422677bdd4d5de825f
emilycw <emily.rose.cw@gmail.com>
2015-11-19 16:57:25 +1100

    make notes for more complex atIndex

--- skiplist.rb
+++ skiplist.rb
@@ -1,11 +1,11 @@
 class SkipList
   def initialize(n)
-    @levels = []*n
-    @n = n
+    @levels = []*(n-1)
+    @totalLevels = (n-1)
   end
 
   # levels are from base upwards
-  # Each item in non 0 levels is [value, (links between it an previous one for each level)]
+  # Each item in non 0 levels is [value, (links between it and next one for each level)]
 
   def makeFromList(list)
     result = SkipList.new()
@@ -15,15 +15,39 @@ class SkipList
     result
   end
 
-  def insert(item)
+  def atIndex(idx)
+    @levels[0][idx]
+    # lev = @totalLevels
+    # cIdx = 0
+    # result = nil
+    # while result.nil?
+    #   if @levels[lev].empty?
+    #     result = -1 if lev== 0
+    #     lev -= 1
+    #   end
+
+    #   if idx == 0
+    #     result = @levels[lev][0][0] 
+    #   elsif (idx + 1) < @levels[lev][0][1][0] 
+    #     # drop down a level, keep going
+    #     # lev -= 1
+    #     # cIdx = 
+    #     # @levels[lev][0][1][0] 
+    #   else
+    #     # go one across on this level, keep going
+    #   end
+    # end
+    # result
   end
 
   def include?(item)
   end
 
+  def insert(item)
+  end
+
   def delete(item)
   end
 
-  def atIndex(idx)
-  end
+  
 end
