adbbcfec8fb4c39d8fb348ef04441548ad7d9633
emilycw <emily.rose.cw@gmail.com>
2015-11-20 17:13:56 +1100

    finish include on SkipListNode

--- skiplistnode.rb
+++ skiplistnode.rb
@@ -1,4 +1,5 @@
 class SkipListNode
+  attr_reader :value
   def initialize(value, downNode, rightNode)
     @value = value
     @downNode = downNode
@@ -6,6 +7,14 @@ class SkipListNode
   end
 
   def include?(item)
+    return true if item == @value
+    return false if item > @value
+    return false if @rightNode.nil?
+    if @rightNode.value > item
+      return false if @downNode.nil
+      return @downNode.include?(item)
+    end
+    @rightNode.include?(item)
   end
 
   def insert(item)
