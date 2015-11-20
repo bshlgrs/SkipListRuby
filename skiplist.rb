2e3db3d7b3aa84832c04d6c1c5cbfdd002e9c7ed
emilycw <emily.rose.cw@gmail.com>
2015-11-20 17:05:58 +1100

    add SkipListNode class

--- skiplist.rb
+++ skiplist.rb
@@ -1,11 +1,9 @@
-class SkipList
-  def initialize(n)
-    @levels = []*(n-1)
-    @totalLevels = (n-1)
-  end
+require "./skiplistnode.rb"
 
-  # levels are from base upwards
-  # Each item in non 0 levels is [value, (links between it and next one for each level)]
+class SkipList
+  def initialize()
+    @firstNode = SkipListNode.new(nil, nil, nil)
+  end
 
   def makeFromList(list)
     result = SkipList.new()
@@ -13,31 +11,6 @@ class SkipList
       result.insert(item)
     end
     result
-  end
-
-  def atIndex(idx)
-    @levels[0][idx]
-    # lev = @totalLevels
-    # cIdx = 0
-    # result = nil
-    # while result.nil?
-    #   if @levels[lev].empty?
-    #     result = -1 if lev== 0
-    #     lev -= 1
-    #   end
-
-    #   if idx == 0
-    #     result = @levels[lev][0][0] 
-    #   elsif (idx + 1) < @levels[lev][0][1][0] 
-    #     # drop down a level, keep going
-    #     # lev -= 1
-    #     # cIdx = 
-    #     # @levels[lev][0][1][0] 
-    #   else
-    #     # go one across on this level, keep going
-    #   end
-    # end
-    # result
   end
 
   def include?(item)
@@ -48,6 +21,5 @@ class SkipList
 
   def delete(item)
   end
-
   
 end
---
+++ skiplistnode.rb
@@ -0,0 +1,17 @@
+class SkipListNode
+  def initialize(value, downNode, rightNode)
+    @value = value
+    @downNode = downNode
+    @rightNode = rightNode
+  end
+
+  def include?(item)
+  end
+
+  def insert(item)
+  end
+
+  def delete(item)
+  end
+  
+end
